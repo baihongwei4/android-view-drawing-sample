a9bf164d4d537ddaaefe5fc8bbc979b46f71c2a1
Mike Hongwei Bai <hongwei.bai@outware.com.au>
2019-07-29 11:18:02 +1000

    add drawing class and related init

--- app/src/main/java/com/hongwei/android_view_drawing_sample/MainActivity.kt
+++ app/src/main/java/com/hongwei/android_view_drawing_sample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.hongwei.android_view_drawing_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun updateDrawing() {
+        view_drawing.progress = 0.4f
+        view_drawing.invalidate()
     }
 }
